ed1f78fee9a453d8fb5c48ca6dec550caba90bc8
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-23 10:05:41 +0530

    Adding TodoDatabase class

---
+++ app/src/main/java/com/satyamthakur/learning/todoapp/database/TodoDatabase.kt
@@ -0,0 +1,33 @@
+package com.satyamthakur.learning.todoapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
+abstract class TodoDatabase : RoomDatabase() {
+
+    abstract fun todoDao(): TodoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoDatabase? = null
+
+        fun getInstance(context: Context): TodoDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TodoDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
