aeb23e5adca9bed83f2fc58ac521a8b448e6eddc
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-23 10:02:08 +0530

    Addin coroutines dependency

--- app/src/main/java/com/satyamthakur/learning/todoapp/database/Todo.kt
+++ app/src/main/java/com/satyamthakur/learning/todoapp/database/Todo.kt
@@ -2,9 +2,11 @@ package com.satyamthakur.learning.todoapp.database
 
 import android.icu.text.CaseMap.Title
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "todos")
 data class Todo(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val description: String,
