5ebcb32fa7cc7cc1060adb6eb535712ace04790a
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-23 10:04:19 +0530

    Adding TodoDao

---
+++ app/src/main/java/com/satyamthakur/learning/todoapp/database/TodoDao.kt
@@ -0,0 +1,15 @@
+package com.satyamthakur.learning.todoapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM todos ORDER BY ID DESC")
+    suspend fun getAllTodo(): LiveData<List<Todo>>
+
+    @Insert
+    suspend fun insertTodo(todo: Todo): Long
+}
